48848d4a0dd7c0fbcc540210b2d59f69b159782a
hannovdw <hannovdw1998@gmail.com>
2020-11-10 11:35:08 +0200

    commit 2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(compression());
 //FILE TYPE CLASSIFICATION........................
 function fileType(){
     let namedir = "public";
-    let fileext = "";
+    let fileext = "none";
     let files = fs.readdirSync(namedir);
     filename = files[0];
     fileext = files[0].split('.').pop();
